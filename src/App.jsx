d60caa78cc2812fb3345339a89edf77e2de8c147
Sujoy Kumar Singha <sujoyscs@gmail.com>
2025-07-11 16:49:17 +0530

    UI : Landing Page Created

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,18 @@ import './App.css'
 import LandingPage from './components/shared/landingpage/LandingPage'
 import AuthPage from './components/main/authPage/AuthPage';
 
-
 function App() {
   // const [setuser]=useAuth()
-  
 
-
-   return (
+  return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<AuthPage />} />
+
       </Routes>
     </Router>
-
-  
-   )
+  )
 }
 
 export default App
